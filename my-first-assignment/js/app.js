dcaa86e49de85cd4ea0baada1d7981b4ec22abaf
steverino33 <steve.f@megacatstudios.com>
2022-11-08 20:20:14 -0500

    modified structure

---
+++ my-first-assignment/js/app.js
@@ -0,0 +1,14 @@
+let myBtn = document.querySelector('.btn');
+
+// 1.
+// function hello(){
+//     alert('You clicked the button!!!')
+// }
+// myBtn.addEventListener('click', hello);
+
+// 2.
+// myBtn.addEventListener('click', function() {alert('You clicked the button!!!')});
+
+// 3. arrow function
+myBtn.addEventListener('click', () => {alert('You clicked the button!!!')});
+
