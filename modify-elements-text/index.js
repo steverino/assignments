d5fe27914782ca1c72975c789e880e84c7871728
steverino33 <steve.f@megacatstudios.com>
2022-11-09 23:57:15 -0500

    init

---
+++ modify-elements-text/index.js
@@ -0,0 +1,16 @@
+// innerHTML - security risk
+// innerText - "expensive"
+// textContent
+
+// var person = {name: "Joe"}
+// person.name = "Jim"
+// console.log(person)
+
+/**
+ * 1. Select the h1 element
+ * 2. Save the h1 element's text to a variable
+ * 3. Change the paragraph element's text to be the same as the text inside the h1
+ */
+let h1Text = document.querySelector('h1').textContent;
+document.querySelector('p').textContent = h1Text;
+// console.log(h1Text);
