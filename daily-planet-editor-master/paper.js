c755d74488f0562d355a0cd4c7062c00127a6e1c
steverino33 <steve.f@megacatstudios.com>
2022-11-09 23:18:38 -0500

    init

---
+++ daily-planet-editor-master/paper.js
@@ -0,0 +1,60 @@
+/*
+This code contains only syntax and code style problems. The logic of the code works,
+so DO NOT change the functionality of anything in here.
+
+In short, you shouldn't need to add your own statements anywhere,
+just fix the existing ones.
+*/
+
+// Written by Kent, Clark
+
+let enemies = [
+  "Lex",
+  "Batman",
+  "Darkseid",
+  "Brainiac",
+  "General Zod",
+  "Doomsday",
+];
+
+function whoWins(is_there_kryptonite, enemy_name) {
+  if (!is_there_kryptonite) {
+    return "Superman beats " + enemy_name + ", of course";
+  } else {
+    return (
+      "Depends on how quick Superman can get rid of the Kryptonite. " +
+      enemy_name +
+      " could possibly win this one."
+    );
+  }
+}
+
+for (var i = 0; i < enemies.length; i++) {
+  var is_there_kryptonite;
+  if (i % 2 === 0) {
+    is_there_kryptonite = true;
+  } else {
+    is_there_kryptonite = false;
+  }
+  console.log(whoWins(is_there_kryptonite, enemies[i]));
+}
+
+function howAttractedIsLoisLaneToMe() {
+  // 1 is not at all attracted, 10 is "super" attracted...
+  return Math.floor(Math.random() * 10 + 1);
+}
+
+console.log(howAttractedIsLoisLaneToMe());
+
+let clarkKent = true;
+let superman = false;
+
+while (clarkKent) {
+  console.log("I'm just a nerdy columnist");
+  let phoneBoothQuickChange = Math.random();
+  if (phoneBoothQuickChange >= 0.5) {
+    clarkKent = false;
+    superman = true;
+    console.log("Now I'm Superman!");
+  }
+}
