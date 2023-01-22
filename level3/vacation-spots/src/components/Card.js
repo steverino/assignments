cf70cd0ce5553ca0a4bfdb1ccd5d1b0d15ee1a23
steverino33 <steve.f@megacatstudios.com>
2023-01-22 17:01:03 -0500

    dollar signs

--- level3/vacation-spots/src/components/Card.js
+++ level3/vacation-spots/src/components/Card.js
@@ -3,13 +3,14 @@ import React from "react";
 export default function Card(props){
     let changeBg;
     let changeColor;
+    let dollarSign;
     if(props.timeToGo === 'Spring'){
         // document.querySelector('.card').classList.add('spring')
         changeBg = 'spring'
     }else if(props.timeToGo === 'Winter' ){
         // document.querySelector('.card').classList.add('winter')
         changeBg = 'winter'
-       changeColor = 'black-text'
+        changeColor = 'black-text'
     }else if(props.timeToGo === 'Summer' ){
         // document.querySelector('.card').classList.add('summer')
         changeBg = 'summer'
@@ -19,15 +20,27 @@ export default function Card(props){
     }else{
         document.querySelector('.card').classList.add('black')
     }
+
+    if(props.price > 1000){
+        dollarSign = '$$$'
+    }else if(props.price < 1000 && props.price > 499 ){
+        dollarSign = '$$'
+    }else if(props.price < 500 ){
+        dollarSign = '$'
+    }else{
+        dollarSign = ''
+    }
+
     return (
         <div className={`card ${changeBg} ${changeColor}`}>
+            
             <div className="card-image">
                 <img src={props.img} alt="" />
             </div>
-
-            <h3 className="place">{props.place} {props.id}</h3>
+            <h3 className="place">{props.place}</h3>
             <p className="price">Price: {props.price}</p>
             <p className="timeToGo">Time to Go: {props.timeToGo}</p>
+            <div className="card--badge">{dollarSign}</div>
         </div>
     )
 }
--- level3/vacation-spots/src/components/CardList.js
+++ level3/vacation-spots/src/components/CardList.js
@@ -6,6 +6,7 @@ export default function CardList() {
   const cards = data.map(function (card) {
     return (
       <div className="card-list">
+        
         <Card
           key={card.id}
           place={card.place}
