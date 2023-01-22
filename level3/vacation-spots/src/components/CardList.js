f196e5a2f2ead4b0b04028dc2571f229f9ab5783
steverino33 <steve.f@megacatstudios.com>
2023-01-22 13:42:12 -0500

    css

--- level3/vacation-spots/src/components/CardList.js
+++ level3/vacation-spots/src/components/CardList.js
@@ -3,10 +3,12 @@ import Card from "./Card";
 import data from "../data.js";
 
 export default function CardList() {
+
+    
   const cards = data.map(function (card) {
     return (
       <div className="card-list">
-        <Card place={card.place} price={card.price} timeToGo={card.timeToGo} />
+        <Card place={card.place} price={card.price} timeToGo={card.timeToGo} img={card.img}/>
       </div>
     );
   });
