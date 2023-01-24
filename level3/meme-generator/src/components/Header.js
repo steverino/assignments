d52ea17d09e2e5f9eb1e16c4b82602c002a24a35
steverino33 <steve.f@megacatstudios.com>
2023-01-24 18:05:02 -0500

    memejs

---
+++ level3/meme-generator/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(){
+    return (
+        <div className="header">
+            <div className="title-pkg">
+            
+            <img className="title--img" src="../Troll Face.png" alt="" />
+            <div className="title"> Meme Generator</div>
+            </div>
+            <nav className="sub-title">React - project 3</nav>
+        </div>
+    )
+}
---
+++ level3/meme-generator/src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme(){
+    return (
+        <div className="meme-form">
+            <form action="">
+                <input type="text" />
+                <input type="text" />
+                <button className="meme-btn">Get a new meme image</button>
+            </form>
+        </div>
+    )
+}
