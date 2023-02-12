7c9aa694d07b4c1d0e8bb438936e571d37555636
steverino33 <steve.f@megacatstudios.com>
2023-02-12 13:37:21 -0500

    alt

--- level3/meme-generator/src/components/Meme.js
+++ level3/meme-generator/src/components/Meme.js
@@ -1,42 +1,27 @@
-import React from "react"
-import memesData from "../memesData.js"
+import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
-   
-let images = memesData.data.memes;
+  let images = memesData.data.memes;
 
-const [randImage, setRandImage] = React.useState([images])
+  const [randImage, setRandImage] = React.useState([images]);
 
-function randomImage(){
-    let randImg = images[Math.floor(Math.random()*images.length)]
-    setRandImage(randImg.url)
-}
-    
-     
-    return (
-        <main>
-            
-            <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                />
-                <button 
-                    className="form--button"
-                    onClick={randomImage}
-                >
-                    Get a new meme image 🖼
-                </button>
-            </div>
-                <div className="meme--image">
-                    <img src={randImage} alt="random" />
-                </div>
-        </main>
-    )
+  function randomImage() {
+    let randImg = images[Math.floor(Math.random() * images.length)];
+    setRandImage(randImg.url);
+  }
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={randomImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme--image">
+        <img src={randImage} alt="random" />
+      </div>
+    </main>
+  );
 }
