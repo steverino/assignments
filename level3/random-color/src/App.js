df54000f0b0b1325b35802eb5de74abde977be0e
steverino33 <steve.f@megacatstudios.com>
2023-02-12 10:08:37 -0500

    title

--- level3/random-color/src/App.js
+++ level3/random-color/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import RandomBg from "./components/RandomBg";
 
 
 
@@ -6,7 +7,7 @@ import React from "react";
 export default function App() {
     return(
         <>
-              
+        <RandomBg/>      
         </>
     )
 }
--- level3/random-color/src/components/RandomBg.js
+++ level3/random-color/src/components/RandomBg.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import axios from 'axios'
 
 const RandomBg = () => {
-  return (
-    <div className='random-bg'>RandomBg
+  const [color, setColor] = React.useState('eee')
+  React.useEffect(() => {
+      axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`).then((response) => {
+          
+          setColor(response.data.colors[0].hex)
+          
+   
+      });
+   
 
-    </div>
+},[])
+    return (
+        <div className='random-bg' style={{ backgroundColor: `#${color}` }}>RandomBg
+        
+        
+        </div>
   )
 }
 
