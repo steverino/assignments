93df79fd7e3814a4f99704efaf009ebf8c3043c0
steverino33 <steve.f@megacatstudios.com>
2023-02-23 15:46:51 -0500

    space

--- level3/caesar-cipher/app.js
+++ level3/caesar-cipher/app.js
@@ -1,4 +1,37 @@
-var readline = require('readline-sync');
-var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
-var shift = parseInt(readline.question('How many letters would you like to shift? '));
+// 65 - 90 Uppercase 97 - 122 lowercase
+
+const readlineSync = require("readline-sync");
+
+let input = readlineSync
+  .question("What phrase would you like to encrypt? ")
+  .toLowerCase();
+let shift = parseInt(
+  readlineSync.question("How many letters would you like to shift? ")
+);
+let spreadInput = [...input];
+let newLetter = []
+
+spreadInput.map((letter) => {
+  let letterCode = letter.charCodeAt(0); //get code for each letter it will be zero index
+  
+  let newLetterCode;
+  if(letterCode === 32){
+    newLetterCode = letterCode
+  }else{
+    newLetterCode = letterCode + shift; // add shift amount to each charCode result
+
+  if (newLetterCode < 97) {
+    // If new letter goes below "a" character code 97
+    newLetterCode += 26;
+  } else if (newLetterCode > 122) {
+    // If new letter goes past "z" character code 122
+    newLetterCode -= 26;
+  }
+}
+  newLetter += [String.fromCharCode(newLetterCode)];
+  
+  
+  
+});
+console.log(newLetter);
 
